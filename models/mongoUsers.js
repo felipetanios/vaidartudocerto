1c59b7400c28283bcfdb5cd0f2adcf8e399434de
felipetanios <felipe.tanios@gmail.com>
2017-05-29 15:46:30 -0300

    adicionada a parte do usuario

---
+++ models/mongoUsers.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+
+usermongo = mongoose.createConnection('mongodb://localhost:27017/UsersDB');
+
+var Schema = mongoose.Schema;
+
+var alunoSchema = new Schema({
+    "user": String,
+    "password": String
+});
+
+module.exports = usermongo.model('Alunos', alunoSchema);
+
+/*
+{"user": "felipe", "password": "senha"}
+{"user": "feec", "password": "pleasedont"}
+*/

782820b90bd0021f64e2b46cc7212eee416cddc0
felipetanios <felipe.tanios@gmail.com>
2017-07-04 15:24:18 -0300

    adicionado os testes dos casos de uso de autenticação e cadatro de usuario

--- spec/api/cadastrar_spec.js
+++ spec/api/cadastrar_spec.js
@@ -1,16 +1,30 @@
 var frisby = require('frisby');
 frisby.create('Testa o fluxo principal do caso de uso de cadastrar usuario')
-    .post('http://localhost:3000/usersignup', {
-        user: "hurleyb",
-        password: "barreiro"
+    .post('http://localhost:3000/authentication', {
+      user: "hurleyb",
+      password: "barreiro"
     })
-    .expectStatus(200)
-    .expectHeaderContains('Content-Type', 'application/json')
-    .expectJSON({
-        resultado: "Usuario cadastrado"
+    .after(function(err, res, body){
+      frisby.create('Apos logar, podemos remover o usuario')
+      .delete('http://localhost:3000/usersignup', {
+          user: "hurleyb",
+          password: "barreiro"
+      })    
+      .after(function(err, res, body){
+        frisby.create('Agora cadastramos o usuario')
+        .post('http://localhost:3000/usersignup', {
+            user: "hurleyb",
+            password: "barreiro"
+        })
+        .expectStatus(200)
+        .expectHeaderContains('Content-Type', 'application/json')
+        .expectJSON({
+            resultado: "Usuario cadastrado"
+        })
+      })
     })
     .toss();
-    
+
 frisby.create('Testa fluxo alternativo do caso de uso de cadastrar usuario para usuario ja cadastrado')
     .post('http://localhost:3000/usersignup', {
         user: "hurleyb",
@@ -22,18 +36,22 @@ frisby.create('Testa fluxo alternativo do caso de uso de cadastrar usuario para 
         resultado: "Usuario ja existente"
     })
     .toss();
-    
+
 frisby.create('Testa o fluxo principal do caso de uso de remover usuario')
-    .delete('http://localhost:3000/usersignup', {
-        user: "hurleyb",
-        password: "barreiro"
+    .post('http://localhost:3000/authentication', {
+      user: "hurleyb",
+      password: "barreiro"
     })
-    .expectStatus(200)
-    .expectHeaderContains('Content-Type', 'application/json')
-    .expectJSON({
-        resultado: "Usuario removido"
+    .after(function(err, res, body){
+      frisby.create('Apos logar, podemos remover o usuario')
+      .delete('http://localhost:3000/usersignup', {
+          user: "hurleyb",
+          password: "barreiro"
+      })
+      .expectStatus(200)
+      .expectHeaderContains('Content-Type', 'application/json')
+      .expectJSON({
+          resultado: "Usuario removido"
+      })
     })
     .toss();
-    
-
-        
--- spec/api/login_spec.js
+++ spec/api/login_spec.js
@@ -1,16 +1,55 @@
 var frisby = require('frisby');
 frisby.create('Testa o fluxo principal do caso de uso de login de usuario')
+    .post('http://localhost:3000/usersignup', {
+        user: "hurleyb",
+        password: "barreiro"
+    })
+    .after(function(err, res, body){
+      frisby.create('Com usuario cadastrado podemos logar')
+          .post('http://localhost:3000/authentication', {
+              user: "hurleyb",
+              password: "barreiro"
+          })
+          .expectStatus(200)
+          .expectHeaderContains('Content-Type', 'application/json')
+          .expectJSON({
+              resultado: "Usuario logado com sucesso"
+          })
+    })
+    .toss();
+
+frisby.create('Testa o fluxo principal do caso de uso de logout de usuario')
+.post('http://localhost:3000/usersignup', {
+    user: "hurleyb",
+    password: "barreiro"
+})
+.after(function(err, res, body){
+  frisby.create('Com usuario cadastrado podemos logar')
     .post('http://localhost:3000/authentication', {
         user: "hurleyb",
         password: "barreiro"
     })
+  .after(function(err, res, body){
+    frisby.create('agora podemos fazer log off')
+    .delete('http://localhost:3000/authentication')
     .expectStatus(200)
     .expectHeaderContains('Content-Type', 'application/json')
     .expectJSON({
-        resultado: "Usuario logado com sucesso"
+        resultado: "Sucesso"
     })
-    .toss();
-    
+  })
+})
+  .toss();
+
+frisby.create('Testa o fluxo alternativo do caso de uso de logout de usuario, para sem ter login')
+  .delete('http://localhost:3000/authentication')
+  .expectStatus(200)
+  .expectHeaderContains('Content-Type', 'application/json')
+  .expectJSON({
+      resultado: "Clique em LOGIN para continuar"
+  })
+  .toss();
+
 frisby.create('Testa o fluxo alternativo do caso de uso de login de usuario para usuario inexistente')
     .post('http://localhost:3000/authentication', {
         user: "stan",
@@ -22,7 +61,7 @@ frisby.create('Testa o fluxo alternativo do caso de uso de login de usuario para
         resultado: "Usuario nao existente"
     })
     .toss();
-    
+
 frisby.create('Testa o fluxo alternativo do caso de uso de login de usuario para senha incorreta')
     .post('http://localhost:3000/authentication', {
         user: "hurleyb",
@@ -32,5 +71,24 @@ frisby.create('Testa o fluxo alternativo do caso de uso de login de usuario para
     .expectHeaderContains('Content-Type', 'application/json')
     .expectJSON({
         resultado: "Usuario ou senha inválidos"
+    })
+    .toss();
+
+frisby.create('Testa o fluxo principal do caso de uso de remover usuario')
+    .post('http://localhost:3000/authentication', {
+      user: "hurleyb",
+      password: "barreiro"
+    })
+    .after(function(err, res, body){
+      frisby.create('Apos logar, podemos remover o usuario')
+      .delete('http://localhost:3000/usersignup', {
+          user: "hurleyb",
+          password: "barreiro"
+      })
+      .expectStatus(200)
+      .expectHeaderContains('Content-Type', 'application/json')
+      .expectJSON({
+          resultado: "Usuario removido"
+      })
     })
     .toss();
